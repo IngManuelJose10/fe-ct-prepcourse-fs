63d92108e1d208c67c08c6bc2f90fc4a7665333a
Manueljose <mcastilloj18gmail.com>
2023-02-16 09:32:41 -0500

    Terminando ejercicios practica modulo 6

--- M05 JavaScript Arrays/homework.js
+++ M05 JavaScript Arrays/homework.js
@@ -184,12 +184,7 @@ function continueStatement(num) {
       }
    }
    return nuevoArray;
-   // Iterar en un bucle aumentando en 2 el número recibido hasta un límite de 10 veces.
-   // Guardar cada nuevo valor en un array y retornarlo.
-   // Cuando el número de iteraciones alcance el valor 5, no se suma ese caso y
-   // se continua con la siguiente iteración.
-   // [PISTA]: utiliza el statement 'continue'.
-   // Tu código:
+
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
